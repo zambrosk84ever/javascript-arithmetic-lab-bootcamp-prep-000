fe9b07e0f8d54aa17328eb471e07d49c0fc05c7c
christopher zambroski <zambrosk84ever@gmail.com>
2018-02-19 02:02:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+function add (a, b) {
+  return (a + b)}
+    (5,3)
+    
+function subtract (b, a) {
+  return (b - a)}
+   (19, 12)
+   
+function multiply (a, b) {
+  return (a * b)}
+  (2,2)
+  
+function divide (a, b) {
+  return (a / b)}
+  (6,2)
+  
+function inc (a) {
+  return (a + 1)}
+  (3)
+  
+function dec (a) {
+  return (a - 1)}
+  (3)
+
+///nan///
+function makeInt(n){
+  return parseInt()}
+  
+function makeInt(n){
+  return parseInt('2', 10)
+}
+
+
+  
+
+  
+
